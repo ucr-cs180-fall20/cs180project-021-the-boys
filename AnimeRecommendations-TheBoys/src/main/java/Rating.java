260c8832796725a5626637a1956e42bf4153a7bc
Syvrus <btcorbita@gmail.com>
2020-10-28 21:45:36 -0700

    Create Rating.java
    >Created Rating.java class for other rating csv file.

---
+++ AnimeRecommendations-TheBoys/src/main/java/Rating.java
@@ -0,0 +1,21 @@
+public class Rating {
+    private int user_id;
+    private int anime_id;
+    private int rating;
+
+    public Rating(){
+
+    }
+
+    Rating(int _anime_id, int _user_id, int _rating){
+        user_id = _user_id;
+        anime_id = _anime_id;
+        rating = _rating;
+    }
+
+    public int getAnime_id(){return anime_id;}
+
+    public int getUser_id(){return user_id;}
+
+    public int getRating(){return rating;}
+}
