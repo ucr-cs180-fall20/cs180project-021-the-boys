82d3585648e94ffd35d6289f5452d8bbdefc1787
Raoul <6421655+Raove@users.noreply.github.com>
2020-11-10 18:12:47 -0800

    Update main.java

--- AnimeRecommendations-TheBoys/src/main/java/main.java
+++ AnimeRecommendations-TheBoys/src/main/java/main.java
@@ -1,14 +1,22 @@
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.MessageBuilder;
+import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.react.GenericMessageReactionEvent;
+import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
 import javax.security.auth.login.LoginException;
+import java.util.Objects;
 
 public class main extends ListenerAdapter {
     private static JDA jda;
     private static Operations op;
+
+    Message testMessage;
+    Message animeListEmbed;
 
     public static void main(String[] args) throws  LoginException {
         op = new Operations();
@@ -28,7 +36,10 @@ public class main extends ListenerAdapter {
                 event.getChannel().sendMessage("Hello World!").complete();
             }
             if(event.getMessage().getContentRaw().startsWith("top")){
-                op.animeListEmbed(event);
+                MessageEmbed temp = op.animeListEmbed(event.getMessage().getContentRaw(),true, false, false);
+                animeListEmbed = event.getChannel().sendMessage(temp).complete();
+                animeListEmbed.addReaction("U+2b05").complete();
+                animeListEmbed.addReaction("U+27a1").complete();
             }
             if(event.getMessage().getContentRaw().startsWith("!") || event.getMessage().getContentRaw().startsWith("$")){
                 op.searchFunction(event);
@@ -72,6 +83,37 @@ public class main extends ListenerAdapter {
             if(event.getMessage().getContentRaw().startsWith("testpiechart")){
                 op.testPieChart(event);
             }
+            if(event.getMessage().getContentRaw().startsWith("testreact")){
+                testMessage = event.getChannel().sendMessage("this should have a reaction on it").complete();
+                testMessage.addReaction("U+2b05").complete();
+                testMessage.addReaction("U+27a1").complete();
+            }
+        }
+    }
+
+    @Override
+    public void onGenericMessageReaction(@NotNull GenericMessageReactionEvent event) {
+        try {
+            if (!event.getUser().isBot()) {
+                if (event.getMessageId().equals(animeListEmbed.getId())) {
+                    if (event.getReactionEmote().getAsCodepoints().equals("U+2b05")) {
+                        MessageEmbed temp = op.animeListEmbed("", false, false, true);
+                        animeListEmbed = animeListEmbed.editMessage(temp).complete();
+                    } else if (event.getReactionEmote().getAsCodepoints().equals("U+27a1")) {
+                        MessageEmbed temp = op.animeListEmbed("", false, true, false);
+                        animeListEmbed = animeListEmbed.editMessage(temp).complete();
+                    }
+                } else if (event.getMessageId().equals(testMessage.getId())) {
+                    if (event.getReactionEmote().getAsCodepoints().equals("U+2b05")) {
+                        event.getChannel().sendMessage("Emote left").complete();
+                    } else if (event.getReactionEmote().getAsCodepoints().equals("U+27a1")) {
+                        event.getChannel().sendMessage("Emote right").complete();
+                    }
+                }
+            }
+        }
+        catch (Exception e){
+
         }
     }
 }
