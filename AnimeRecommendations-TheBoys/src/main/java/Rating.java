2dc4b1e4819e2bdd9195b9499814de028b0aba1f
Raoul <6421655+Raove@users.noreply.github.com>
2020-11-18 17:49:11 -0800

    Update Rating.java

--- AnimeRecommendations-TheBoys/src/main/java/Rating.java
+++ AnimeRecommendations-TheBoys/src/main/java/Rating.java
@@ -1,22 +1,23 @@
 public class Rating {
-    private int user_id;
-    private int anime_id;
-    private int rating;
+    private String user_id;
+    private String anime_id;
+    private String rating;
 
     public Rating(){
 
     }
 
-    Rating(int _anime_id, int _user_id, int _rating){
+    Rating(String _anime_id, String _user_id, String _rating){
         user_id = _user_id;
         anime_id = _anime_id;
         rating = _rating;
     }
 
-    public int getAnime_id(){return anime_id;}
+    public int getAnime_id(){return Integer.parseInt(anime_id);}
 
-    public int getUser_id(){return user_id;}
+    public int getUser_id(){return Integer.parseInt(user_id);}
 
-    public int getRating(){return rating;}
-    public void setRating(int d){rating = d;}
+    public int getRating(){return Integer.parseInt(rating);}
+
+    public void setRating(int d){rating = Integer.toString(d);}
 }
